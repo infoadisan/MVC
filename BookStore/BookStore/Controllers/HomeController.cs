0bd83f035ff0348e0fadedc7f52561b314d034e3
Infoadisan <info.adisan@gmail.com>
2023-07-13 18:01:14 +0530

    views

--- BookStore/BookStore/Controllers/HomeController.cs
+++ BookStore/BookStore/Controllers/HomeController.cs
@@ -1,11 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Dynamic;
 
 namespace BookStore.Controllers
 {
     public class HomeController : Controller
     {
+        [ViewData]
+        public string CustomProperty { get; set; }
+        [ViewData]
+        public string Title { get; set; }
         public ViewResult Index()
         {
+            ViewBag.title = "QWE";
+
+            dynamic data = new ExpandoObject();
+            data.Id = 1;
+            data.Name = "Nitish";
+
+            ViewBag.Data = data;
+
+            ViewData["book"] = new BookModel()
+            {
+                Id = 003,
+                Author = "new",
+                Description = "fdf"
+            };
+
+            //view data attributes
+            CustomProperty = "Custom Value";
+            Title = "Home from Controller";
+
+
             return View();
         }
 
