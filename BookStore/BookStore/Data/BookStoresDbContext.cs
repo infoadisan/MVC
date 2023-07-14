ec120de1dee11f0b5809ccdb5526930d8ef0cf76
Sanjeev Choudhary <sanjeev.choudhary@adisan.in>
2023-07-14 20:30:30 +0530

    A7 scaffold db done

---
+++ BookStore/BookStore/Data/BookStoresDbContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using BookStore.Data.Models;
+
+namespace BookStore.Data
+{
+    public partial class BookStoresDbContext : DbContext
+    {
+        public BookStoresDbContext()
+        {
+        }
+
+        public BookStoresDbContext(DbContextOptions<BookStoresDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Book> Books { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=BookstoresDBCore;Integrated Security=True;Encrypt=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
