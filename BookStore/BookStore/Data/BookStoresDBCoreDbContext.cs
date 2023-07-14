82f7f0062f195e5fe46583182f1f375fd07956e1
Infoadisan <info.adisan@gmail.com>
2023-07-14 16:35:19 +0530

    G5_2

---
+++ BookStore/BookStore/Data/BookStoresDBCoreDbContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using BookStore.Data.Models;
+
+namespace BookStore.Data
+{
+    public partial class BookStoresDBCoreDbContext : DbContext
+    {
+        public BookStoresDBCoreDbContext()
+        {
+        }
+
+        public BookStoresDBCoreDbContext(DbContextOptions<BookStoresDBCoreDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Book> Books { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Name=default");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
